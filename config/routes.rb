21c6b0d543f2f77df50b92aeac13606fed814dd7
Marcelo Dias <marcelocdias93@gmail.com>
2023-08-26 23:09:20 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, param: :_username
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/auth/login', to: 'authentication#login'
+
+  get '/*a', to: 'application#not_found'
 end
