c7cde5c98adc818640af779bd374e2461b122aa2
Marcelo Dias <marcelocdias93@gmail.com>
2023-08-26 23:28:42 -0300

    Create authorize_request function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.header['AUTHORIZATION']
+    header = header.split(' ').last if header
+
+    begin
+      @decoded      = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
